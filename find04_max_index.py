74b07ae34bdd7b941d76856657fc386c9bdab28c
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-01-23 22:45:40 +0500

    cod04

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,15 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    mx=data[0]
+    i=0
+    while i<len(data):
+        if mx<data[i]:
+            mx=data[i]
+        
+
+            
+        i+=1
+    return data.index(mx)
+print(find_max_index([12,4,5,3,7,2,9]))
+    
