ad4f8499fa71157a9b597b97e9a84290eb4f72cd
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-01-23 21:56:08 +0500

    cod02

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,12 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    mx=data[0]
+    i=0
+    while i<len(data):
+        if mx<data[i]:
+            s=data[i]
+        i+=1
+    return data.count(mx)
+print(find_max_count([12, 2, 5, 2, 7, 9, 1]))
+    
